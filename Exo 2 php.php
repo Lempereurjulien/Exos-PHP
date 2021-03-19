05a73b72b6f07d211875e06c73be877a1138a12a
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-19 11:08:27 +0100

    Fin exos 2

--- Exo 2 php.php
+++ Exo 2 php.php
@@ -1,7 +1,8 @@
 <?php
 $i =0;
 do{
-    echo 'Je dois apprendre mon coeur';
+    echo 'Je dois apprendre mon cours par coeur';
+    echo ' ';
     $i = $i + 1;
 }
-while(!$i==100);
+while($i!==100);
