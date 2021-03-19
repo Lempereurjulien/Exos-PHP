5d3c9cc597450dd8a290b55c83d992f02597109e
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-19 10:58:41 +0100

    Fin premier exos

--- Exos 1.php
+++ Exos 1.php
@@ -1,8 +1,8 @@
 <?php
-echo "hello world";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$i = 1;
+while($i<10000){  
+    echo $i;
+    echo ' ';
+    $i=$i+2;  
+    }    
+?>
